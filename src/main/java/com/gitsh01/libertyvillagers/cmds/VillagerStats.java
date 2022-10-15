3cc1f9095f9912f1a487c33dcd3ed2e62cd61936
gitsh01 <gitsh01@gmail.com>
2022-10-14 22:37:08 -0700

    Added number of unoccupied beds and unclaimed workstations to villagerstats.

--- src/main/java/com/gitsh01/libertyvillagers/cmds/VillagerStats.java
+++ src/main/java/com/gitsh01/libertyvillagers/cmds/VillagerStats.java
@@ -9,13 +9,35 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
+import net.minecraft.village.VillagerProfession;
+import net.minecraft.world.poi.PointOfInterestStorage;
+import net.minecraft.world.poi.PointOfInterestTypes;
 import org.apache.commons.lang3.tuple.MutablePair;
 
-import java.util.HashMap;
 import java.util.List;
+import java.util.TreeMap;
 
 import static com.gitsh01.libertyvillagers.LibertyVillagersMod.CONFIG;
 import static net.minecraft.server.command.CommandManager.literal;
+
+class ProfessionInfo {
+    public VillagerProfession profession;
+    public int countVillagersWithProfession;
+    public int countVillagersWithWorkstations;
+
+    public ProfessionInfo(VillagerProfession profession, int countVillagersWithProfession,
+                          int countVillagersWithWorkstations) {
+        this.profession = profession;
+        this.countVillagersWithProfession = countVillagersWithProfession;
+        this.countVillagersWithWorkstations = countVillagersWithWorkstations;
+    }
+
+    public static ProfessionInfo mergeProfessionInfo(ProfessionInfo oldVal, ProfessionInfo newVal) {
+        return new ProfessionInfo(oldVal.profession,
+                oldVal.countVillagersWithProfession + newVal.countVillagersWithProfession,
+                oldVal.countVillagersWithWorkstations + newVal.countVillagersWithWorkstations);
+    }
+}
 
 public class VillagerStats {
     public static void register() {
@@ -51,32 +73,48 @@ public class VillagerStats {
                 villagers.size()));
 
         // MutablePair is countOfVillagersWithProfession, countOfVillagersWithProfessionWhoHaveWorkstations.
-        HashMap<String, MutablePair<Integer, Integer>> villagerProfessionMap = new HashMap<>();
+        TreeMap<String, ProfessionInfo> villagerProfessionMap = new TreeMap<>();
         int numHomeless = 0;
         for (VillagerEntity villager : villagers) {
             if (villager.isBaby()) {
                 // TODO: getProfession() is not localized currently. When getProfession is localized, also localize
                 // "baby".
-                villagerProfessionMap.merge("baby", new MutablePair<>(1, 0), VillagerStats::sumPair);
+                villagerProfessionMap.merge("baby",
+                        new ProfessionInfo(villager.getVillagerData().getProfession(), 1, 0),
+                        ProfessionInfo::mergeProfessionInfo);
             } else {
-
-                villagerProfessionMap.merge(villager.getVillagerData().getProfession().toString(), new MutablePair<>(1,
+                villagerProfessionMap.merge(villager.getVillagerData().getProfession().toString(),
+                        new ProfessionInfo(villager.getVillagerData().getProfession(), 1,
                                 villager.getBrain().getOptionalMemory(MemoryModuleType.JOB_SITE).isPresent() ? 1 : 0),
-                        VillagerStats::sumPair);
+                        ProfessionInfo::mergeProfessionInfo);
             }
             if (!villager.getBrain().hasMemoryModule(MemoryModuleType.HOME)) {
                 numHomeless++;
             }
         }
 
+        long numAvailableBeds = serverWorld.getPointOfInterestStorage()
+                .count(registryEntry -> registryEntry.matchesKey(PointOfInterestTypes.HOME), player.getBlockPos(),
+                        CONFIG.villagerStatRange, PointOfInterestStorage.OccupationStatus.HAS_SPACE);
+
         player.sendMessage(Text.translatable("text.LibertyVillagers.villagerStats.format",
-                Text.translatable("text.LibertyVillagers.villagerStats.numberOfHomeless").getString(), numHomeless));
+                Text.translatable("text.LibertyVillagers.villagerStats.numberOfHomeless").getString(), numHomeless,
+                numAvailableBeds));
 
         player.sendMessage(Text.translatable("text.LibertyVillagers.villagerStats.professions"));
-        villagerProfessionMap.forEach((villagerProfession, pair) -> {
+        villagerProfessionMap.forEach((villagerProfession, professionInfo) -> {
+
+            long numAvailableWorkstations = 0;
+            if (villagerProfession != "baby") {
+                numAvailableWorkstations = serverWorld.getPointOfInterestStorage()
+                        .count(professionInfo.profession.acquirableWorkstation(), player.getBlockPos(),
+                                CONFIG.villagerStatRange, PointOfInterestStorage.OccupationStatus.HAS_SPACE);
+            }
+
             player.sendMessage(
                     Text.translatable("text.LibertyVillagers.villagerStats.professionsFormat", villagerProfession,
-                            pair.getLeft(), pair.getRight()));
+                            professionInfo.countVillagersWithProfession, professionInfo.countVillagersWithWorkstations,
+                            numAvailableWorkstations));
         });
     }
 }
