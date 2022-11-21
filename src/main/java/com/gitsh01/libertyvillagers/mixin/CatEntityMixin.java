b1a44c8c321608c024739fee0653befd5b129f1e
gitsh01 <gitsh01@gmail.com>
2022-11-20 22:31:07 -0800

    feat: Added options to make every cat a black cat, and to have black cats spawn regardless of the moon phase.

--- src/main/java/com/gitsh01/libertyvillagers/mixin/CatEntityMixin.java
+++ src/main/java/com/gitsh01/libertyvillagers/mixin/CatEntityMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.CatEntity;
+import net.minecraft.entity.passive.CatVariant;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.world.LocalDifficulty;
@@ -11,14 +12,19 @@ import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static com.gitsh01.libertyvillagers.LibertyVillagersMod.CONFIG;
 
 @Mixin(CatEntity.class)
 public abstract class CatEntityMixin extends TameableEntity {
+
+    @Shadow
+    public abstract void setVariant(CatVariant variant);
 
     public CatEntityMixin(EntityType<? extends CatEntity> entityType, World world) {
         super(entityType, world);
@@ -33,5 +39,21 @@ public abstract class CatEntityMixin extends TameableEntity {
         if (CONFIG.catsConfig.villageCatsDontDespawn) {
             this.setPersistent();
         }
+
+        if (CONFIG.catsConfig.allBlackCats) {
+            this.setVariant(CatVariant.ALL_BLACK);
+        }
+    }
+
+    @Redirect(method = "initialize(Lnet/minecraft/world/ServerWorldAccess;Lnet/minecraft/world/LocalDifficulty;" +
+                    "Lnet/minecraft/entity/SpawnReason;Lnet/minecraft/entity/EntityData;Lnet/minecraft/nbt/NbtCompound;)Lnet/minecraft/entity/EntityData;",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/ServerWorldAccess;getMoonSize()F"))
+    private float replaceMoonSize(ServerWorldAccess world) {
+        if (CONFIG.catsConfig.blackCatsAtAnyTime) {
+            return 1.0f;
+        }
+
+        return world.getMoonSize();
     }
 }
