0b127c55e12a1c39ee1e102d5b611a4de6b2f7ea
gitsh01 <gitsh01@gmail.com>
2022-11-24 16:58:16 -0800

    fix: Pathfinding over 100 blocks is done in smaller chunks.

--- src/main/java/com/gitsh01/libertyvillagers/mixin/VillagerWalkTowardsTaskMixin.java
+++ src/main/java/com/gitsh01/libertyvillagers/mixin/VillagerWalkTowardsTaskMixin.java
@@ -4,13 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import net.minecraft.entity.ai.brain.task.Task;
 import net.minecraft.entity.ai.brain.task.VillagerWalkTowardsTask;
 import net.minecraft.entity.passive.VillagerEntity;
-import net.minecraft.util.math.GlobalPos;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import static com.gitsh01.libertyvillagers.LibertyVillagersMod.CONFIG;
 
 @Mixin(VillagerWalkTowardsTask.class)
 public abstract class VillagerWalkTowardsTaskMixin extends Task<VillagerEntity> {
@@ -18,11 +12,18 @@ public abstract class VillagerWalkTowardsTaskMixin extends Task<VillagerEntity> 
         super(ImmutableMap.of());
     }
 
+    /*
+    Don't enable this function, as using the default exceeds of 100/150 blocks (for meeting/home) is preferred.
+    If the location is too far away the villager uses no penalty targeting to go in the right direction a short
+    distance which prevents the villager from comitting to a path and then getting frustrated when they get stuck.
+    Leaving this comment here to prevent myself from making this mistake again.
     @Inject(method = "exceedsMaxRange(Lnet/minecraft/entity/passive/VillagerEntity;" +
             "Lnet/minecraft/util/dynamic/GlobalPos;)Z", at = @At("HEAD"), cancellable = true)
     private void exceedsMaxRange(VillagerEntity villager, GlobalPos pos, CallbackInfoReturnable<Boolean> cir) {
+        return pos.getPos().getManhattanDistance(villager.getBlockPos()) > this.maxRange;
         cir.setReturnValue(pos.getPos().getManhattanDistance(villager.getBlockPos()) >
                 CONFIG.villagersGeneralConfig.pathfindingMaxRange);
         cir.cancel();
     }
+     */
 }
