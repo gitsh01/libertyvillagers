ee54c7dce01ddfe684f6273310806b55cc337051
gitsh01 <gitsh01@gmail.com>
2022-12-05 21:52:26 -0800

    Fix: Fixed untranslated string "baby" in villager stats.

--- src/main/java/com/gitsh01/libertyvillagers/cmds/VillagerStats.java
+++ src/main/java/com/gitsh01/libertyvillagers/cmds/VillagerStats.java
@@ -220,7 +220,7 @@ public class VillagerStats {
 
         for (VillagerEntity villager : villagers) {
             if (villager.isBaby()) {
-                String babyText = Text.translatable("text.LibertyVillagers.villagerStats.baby").toString();
+                String babyText = Text.translatable("text.LibertyVillagers.villagerStats.baby").getString();
                 villagerProfessionMap.merge(babyText, new ProfessionInfo(villager.getVillagerData().getProfession(), 1),
                         ProfessionInfo::mergeProfessionInfo);
             } else {
