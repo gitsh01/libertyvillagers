98553aa05faab8fab80f5c7d9bc790330f96e10d
gitsh01 <gitsh01@gmail.com>
2022-12-12 22:44:48 -0800

    fix: fix for villagers spamming fuzzy targeting when stuck.

--- src/main/java/com/gitsh01/libertyvillagers/mixin/WanderAroundTaskMixin.java
+++ src/main/java/com/gitsh01/libertyvillagers/mixin/WanderAroundTaskMixin.java
@@ -1,7 +1,7 @@
 package com.gitsh01.libertyvillagers.mixin;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.FuzzyTargeting;
+import net.minecraft.entity.ai.NoPenaltyTargeting;
 import net.minecraft.entity.ai.brain.BlockPosLookTarget;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.ai.brain.WalkTarget;
@@ -41,14 +41,15 @@ public abstract class WanderAroundTaskMixin {
 
     private long previousEntityPosTime;
 
+    private int fuzzyTries;
+
     @Shadow
     abstract boolean hasReached(MobEntity entity, WalkTarget walkTarget);
 
-    @Inject(method = "<init>()V",
-            at = @At("TAIL"))
+    @Inject(method = "<init>()V", at = @At("TAIL"))
     public void replaceFarmerVillagerTaskRunTime(CallbackInfo ci) {
-        ((TaskAccessorMixin)this).setMaxRunTime(MAX_RUN_TIME);
-        ((TaskAccessorMixin)this).setMinRunTime(MAX_RUN_TIME);
+        ((TaskAccessorMixin) this).setMaxRunTime(MAX_RUN_TIME);
+        ((TaskAccessorMixin) this).setMinRunTime(MAX_RUN_TIME);
     }
 
     @Inject(method = "hasFinishedPath(Lnet/minecraft/entity/mob/MobEntity;Lnet/minecraft/entity/ai/brain/WalkTarget;J)Z",
@@ -59,27 +60,23 @@ public abstract class WanderAroundTaskMixin {
     }
 
     private void checkToSeeIfVillagerHasMoved(ServerWorld serverWorld, MobEntity entity, long time) {
-        BlockPos entityPos = new BlockPos(entity.getBlockX(), LandPathNodeMaker.getFeetY(serverWorld,
-                entity.getBlockPos()), entity.getBlockZ());
+        BlockPos entityPos =
+                new BlockPos(entity.getBlockX(), LandPathNodeMaker.getFeetY(serverWorld, entity.getBlockPos()),
+                        entity.getBlockZ());
         if (previousEntityPos == null || !previousEntityPos.isWithinDistance(entityPos, 1)) {
             previousEntityPos = entityPos;
             previousEntityPosTime = time;
-            entity.getBrain().forget(MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE);
-        }
-        if ((time - previousEntityPosTime > STUCK_TIME) && !entity.getBrain().hasMemoryModule(MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE)) {
-            entity.getBrain().remember(MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE, previousEntityPosTime);
+            fuzzyTries = 0;
         }
     }
 
-    @Inject(method = "keepRunning",
-            at = @At("HEAD"))
+    @Inject(method = "keepRunning", at = @At("HEAD"))
     private void keepRunningCheckToSeeIfVillagerHasMoved(ServerWorld serverWorld, MobEntity entity, long time,
-                                                 CallbackInfo ci) {
-        checkToSeeIfVillagerHasMoved(serverWorld,entity, time);
+                                                         CallbackInfo ci) {
+        checkToSeeIfVillagerHasMoved(serverWorld, entity, time);
     }
 
-    @Inject(method = "shouldRun",
-            at = @At("HEAD"))
+    @Inject(method = "shouldRun", at = @At("HEAD"))
     protected void shouldRun(ServerWorld serverWorld, MobEntity entity, CallbackInfoReturnable<Boolean> cir) {
         long time = serverWorld.getTime();
         checkToSeeIfVillagerHasMoved(serverWorld, entity, time);
@@ -87,11 +84,9 @@ public abstract class WanderAroundTaskMixin {
 
     private boolean lastDitchAttemptToFindPath(MobEntity entity, long time) {
         if (CONFIG.villagersGeneralConfig.villagerWanderingFix && entity.getType() == EntityType.VILLAGER &&
-                entity.getBrain().getOptionalMemory(MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE).isPresent() &&
-                previousEntityPosTime > 0) {
-            long cantReachWalkTargetSince = time - previousEntityPosTime;
+                previousEntityPosTime > 0 && (time - previousEntityPosTime > STUCK_TIME)) {
             // Fuzzy pathing has failed, teleport.
-            if (cantReachWalkTargetSince > 8 * 20) {
+            if (fuzzyTries > 3) {
                 Vec3d desiredPos;
                 boolean shouldRun = false;
                 if (this.path != null) {
@@ -103,27 +98,28 @@ public abstract class WanderAroundTaskMixin {
                     desiredPos = new Vec3d(blockPos.getX() + 0.5f, blockPos.getY(), blockPos.getZ() + 0.5f);
                 }
                 entity.teleport(desiredPos.x, desiredPos.y, desiredPos.z, true);
-                entity.getBrain().forget(MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE);
                 this.previousEntityPosTime = 0;
                 return shouldRun;
-            } else if (cantReachWalkTargetSince > 3 * 20) {
+            } else {
                 // Fix for really difficult pathing situations such as the armorer's house in the SkyVillage mod using
                 // fuzzy pathing to wiggle out of the area.
-                Vec3d vec3d = FuzzyTargeting.findTo((PathAwareEntity) entity, 5, 5,
-                        Vec3d.ofBottomCenter(this.walkTarget.getLookTarget().getBlockPos()));
+                BlockPos blockPos = this.walkTarget.getLookTarget().getBlockPos();
+                Vec3d vec3d = NoPenaltyTargeting.findTo((PathAwareEntity) entity, 10, 7, Vec3d.ofBottomCenter(blockPos),
+                        1.5707963705062866);
                 if (vec3d != null) {
                     this.path = entity.getNavigation().findPathTo(vec3d.x, vec3d.y, vec3d.z, 0);
                 }
+                fuzzyTries++;
+                previousEntityPosTime = time;
                 return this.path != null;
             }
         }
         return false;
     }
 
-    @Inject(method = "keepRunning",
-            at = @At("TAIL"))
+    @Inject(method = "keepRunning", at = @At("TAIL"))
     private void keepRunninglastDitchAttemptToFindPath(ServerWorld serverWorld, MobEntity entity, long time,
-                                                 CallbackInfo ci) {
+                                                       CallbackInfo ci) {
         if (this.walkTarget == null) {
             if (entity.getBrain().getOptionalMemory(MemoryModuleType.WALK_TARGET).isEmpty()) {
                 return;
@@ -136,25 +132,25 @@ public abstract class WanderAroundTaskMixin {
     @Inject(method = "hasFinishedPath(Lnet/minecraft/entity/mob/MobEntity;Lnet/minecraft/entity/ai/brain/WalkTarget;J)Z",
             at = @At("RETURN"), cancellable = true)
     private void hasFinishedPathLastDitchAttemptToFindPath(MobEntity entity, WalkTarget walkTarget, long time,
-                                            CallbackInfoReturnable<Boolean> cir) {
+                                                           CallbackInfoReturnable<Boolean> cir) {
         if (lastDitchAttemptToFindPath(entity, time)) {
             cir.setReturnValue(true);
             cir.cancel();
         }
     }
 
-        @ModifyArg(
-                method = "hasFinishedPath(Lnet/minecraft/entity/mob/MobEntity;Lnet/minecraft/entity/ai/brain/WalkTarget;J)Z",
-                at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/pathing/EntityNavigation;findPathTo" +
-                        "(Lnet/minecraft/util/math/BlockPos;I)Lnet/minecraft/entity/ai/pathing/Path;"), index = 1)
-        int replaceDistanceInFindPathToInHasFinishedPath ( int distance){
-            // Fix for villagers being unable to path to POIs where that are surrounded by blocks except for one side.
-            // VillagerWalkTowardsTask uses manhattan distance, FindPathTo uses crow-flies distance. Setting the
-            // distance to 1 means that positions all around the POI are valid, but still within the manhattan distance
-            // of 3 (assuming VillagerWalkTowardsTask uses 3).
-            if (walkTarget.getLookTarget() instanceof BlockPosLookTarget) {
-                return Math.max(1, distance);
-            }
-            return distance;
+    @ModifyArg(
+            method = "hasFinishedPath(Lnet/minecraft/entity/mob/MobEntity;Lnet/minecraft/entity/ai/brain/WalkTarget;J)Z",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/pathing/EntityNavigation;findPathTo" +
+                    "(Lnet/minecraft/util/math/BlockPos;I)Lnet/minecraft/entity/ai/pathing/Path;"), index = 1)
+    int replaceDistanceInFindPathToInHasFinishedPath(int distance) {
+        // Fix for villagers being unable to path to POIs where that are surrounded by blocks except for one side.
+        // VillagerWalkTowardsTask uses manhattan distance, FindPathTo uses crow-flies distance. Setting the
+        // distance to 1 means that positions all around the POI are valid, but still within the manhattan distance
+        // of 3 (assuming VillagerWalkTowardsTask uses 3).
+        if (walkTarget.getLookTarget() instanceof BlockPosLookTarget) {
+            return Math.max(1, distance);
         }
+        return distance;
     }
+}
