0456ae96df5bdde799b76acd57d54ccdf8b3366f
gitsh01 <gitsh01@gmail.com>
2023-01-02 16:37:35 -0800

    fix: Don't "GoToIfNearby" if fishing.

---
+++ src/main/java/com/gitsh01/libertyvillagers/mixin/GoToIfNearbyTaskMixin.java
@@ -0,0 +1,32 @@
+package com.gitsh01.libertyvillagers.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.brain.task.GoToIfNearbyTask;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.village.VillagerProfession;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(GoToIfNearbyTask.class)
+public class GoToIfNearbyTaskMixin {
+
+    @Inject(method = "shouldRun",
+            at = @At("HEAD"),
+            cancellable = true)
+    void dontRunIfFishing(ServerWorld serverWorld, PathAwareEntity pathAwareEntity,
+                          CallbackInfoReturnable<Boolean> cir){
+        if (pathAwareEntity.getType() == EntityType.VILLAGER) {
+            VillagerEntity villager = (VillagerEntity) pathAwareEntity;
+            if (villager.getVillagerData().getProfession() == VillagerProfession.FISHERMAN &&
+                    villager.getMainHandStack().isOf(Items.FISHING_ROD)) {
+                cir.setReturnValue(false);
+                cir.cancel();
+            }
+        }
+    }
+}
