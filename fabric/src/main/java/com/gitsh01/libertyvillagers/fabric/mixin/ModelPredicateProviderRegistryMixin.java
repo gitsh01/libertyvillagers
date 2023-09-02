08b1ca362be8833858fa7787757e825941b94e0e
gitsh01 <gitsh01@gmail.com>
2023-09-01 23:34:31 -0700

    Fixed: Crash in ModelPredicateProviderRegistryMixin. Forge remaps a class from UnclampedModelPredicateProvider back to the deprecated ModelPredicateProvider. For this reason I needed to make a platform specific version of this mixin.

---
+++ fabric/src/main/java/com/gitsh01/libertyvillagers/fabric/mixin/ModelPredicateProviderRegistryMixin.java
@@ -0,0 +1,47 @@
+package com.gitsh01.libertyvillagers.fabric.mixin;
+
+import com.google.common.collect.Maps;
+import net.minecraft.client.item.ModelPredicateProvider;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.client.item.UnclampedModelPredicateProvider;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+
+@Mixin(ModelPredicateProviderRegistry.class)
+public class ModelPredicateProviderRegistryMixin {
+
+    @Shadow
+    @Mutable
+    static Map<Item, Map<Identifier, ModelPredicateProvider>> ITEM_SPECIFIC;
+
+    // Forge insists on remapping this to "modelPredicateProvider" instead of "unclampedModelPredicateProvider". :(
+    // Fabric does it the correct way, but because Forge is being a problem, we need platform specific versions.
+    @Inject(method = "register(Lnet/minecraft/item/Item;Lnet/minecraft/util/Identifier;Lnet/minecraft/client/item/UnclampedModelPredicateProvider;)V",
+            at = @At("HEAD"),
+            cancellable = true)
+    private static void register(Item item, Identifier id, UnclampedModelPredicateProvider provider, CallbackInfo ci) {
+        if (item != Items.FISHING_ROD) {
+            return;
+        }
+        // Villagers should show the used graphic for the fishing rod.
+        UnclampedModelPredicateProvider newProvider = (stack, world, entity, seed) -> {
+            if (entity != null && entity.getType() == EntityType.VILLAGER) {
+                return 1.0f;
+            }
+            return provider.unclampedCall(stack, world, entity, seed);
+        };
+        ITEM_SPECIFIC.computeIfAbsent(item, key -> Maps.newHashMap()).put(id, newProvider);
+        ci.cancel();
+    }
+}
+
--- forge/src/main/java/com/gitsh01/libertyvillagers/forge/mixin/ModelPredicateProviderRegistryMixin.java
+++ forge/src/main/java/com/gitsh01/libertyvillagers/forge/mixin/ModelPredicateProviderRegistryMixin.java
@@ -1,9 +1,8 @@
-package com.gitsh01.libertyvillagers.mixin;
+package com.gitsh01.libertyvillagers.forge.mixin;
 
 import com.google.common.collect.Maps;
 import net.minecraft.client.item.ModelPredicateProvider;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
-import net.minecraft.client.item.UnclampedModelPredicateProvider;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
@@ -24,19 +23,20 @@ public class ModelPredicateProviderRegistryMixin {
     @Mutable
     static Map<Item, Map<Identifier, ModelPredicateProvider>> ITEM_SPECIFIC;
 
-    @Inject(method = "register(Lnet/minecraft/item/Item;Lnet/minecraft/util/Identifier;Lnet/minecraft/client/item/UnclampedModelPredicateProvider;)V",
+    // Forge insists on remapping this to "modelPredicateProvider" instead of "unclampedModelPredicateProvider". :(
+    @Inject(method = "register(Lnet/minecraft/item/Item;Lnet/minecraft/util/Identifier;Lnet/minecraft/client/item/ModelPredicateProvider;)V",
             at = @At("HEAD"),
             cancellable = true)
-    private static void register(Item item, Identifier id, UnclampedModelPredicateProvider provider, CallbackInfo ci) {
+    private static void register(Item item, Identifier id, ModelPredicateProvider provider, CallbackInfo ci) {
         if (item != Items.FISHING_ROD) {
             return;
         }
         // Villagers should show the used graphic for the fishing rod.
-        UnclampedModelPredicateProvider newProvider = (stack, world, entity, seed) -> {
+        ModelPredicateProvider newProvider = (stack, world, entity, seed) -> {
             if (entity != null && entity.getType() == EntityType.VILLAGER) {
                 return 1.0f;
             }
-            return provider.unclampedCall(stack, world, entity, seed);
+            return provider.call(stack, world, entity, seed);
         };
         ITEM_SPECIFIC.computeIfAbsent(item, key -> Maps.newHashMap()).put(id, newProvider);
         ci.cancel();
